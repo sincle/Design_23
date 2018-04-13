4230e2d99623eae2572377f82f06b7fc8f199bf5
kang <agudao@sina.com>
2018-04-13 11:15:12 +0800

    ---------------------------------

---
+++ jsonparse/src/main/java/com/haieros/jsonparse/Adapter.java
@@ -0,0 +1,19 @@
+package com.haieros.jsonparse;
+
+/**
+ * Created by Kang on 2018/4/13.
+ */
+
+public class Adapter implements source1,source2 {
+
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+}
---
+++ jsonparse/src/main/java/com/haieros/jsonparse/source2.java
@@ -0,0 +1,9 @@
+package com.haieros.jsonparse;
+
+/**
+ * Created by Kang on 2018/4/13.
+ */
+
+public interface source2 {
+    void method2();
+}
